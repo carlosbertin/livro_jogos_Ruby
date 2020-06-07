07940a2172894b1ddf6642a2e85ec34f3d65af20
Carlos Bertin <carlosbertin@id.uff.br>
2020-06-07 18:32:51 -0300

    exercícios feitos até o capítulo 5 (inclusive)

---
+++ app/models/maior_ou_menor.rb
@@ -0,0 +1,57 @@
+# rodando no rails c
+# 2.5.0 :001 > require('maior_ou_menor')
+def da_boas_vindas
+    puts "Bem-vindo ao jogo da adivinhação"
+    puts "Qual é o seu nome?"
+    nome = gets
+    puts "\n\n"
+    puts "Começaremos o jogo para você, " + nome
+    puts "\n\n"
+end
+
+def sorteia_numero_secreto
+    puts "Escolhendo um número secreto entre 0 e 200..."
+    sorteado = 175
+    puts "Escolhido... que tal adivinhar hoje nosso número secreto ?"
+    puts "\n\n"
+    sorteado
+end
+
+def pede_um_numero(tentativa, limite_de_tentativas)
+    puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+    puts "Entre com o número"
+    chute = gets
+    puts "Será que você acertou ? Você chutou #{chute}"
+    chute.to_i
+end
+
+def verifica_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute
+
+    if acertou
+        puts "Acertou !"
+        puts "\n\n"
+        return true
+    else
+        maior = numero_secreto > chute
+        if maior
+            puts "O número secreto é maior!"
+        else
+            puts "O número secreto é menor!"
+        end
+        puts "\n\n"
+        false
+    end
+end
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas
+    chute = pede_um_numero(tentativa, limite_de_tentativas)
+    break if verifica_se_acertou(numero_secreto, chute)
+end
+
+
